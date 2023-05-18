24bac113118744610b254cc6047dff186647de7c
gero <ger0@github.com>
2023-05-18 22:23:20 +0200

    added a connection manager

--- server.py
+++ server.py
@@ -1,40 +1,86 @@
 from fastapi import FastAPI, WebSocket
 from enum import Enum
 import struct
+import json
 
-
-State = Enum('State', ['EMPTY', 'BLACK', 'WHITE'])
+# default values
 MAP_S = 8
-
-
-class Board:
-    def __init__(self):
-        self.data = [State.BLACK] * (MAP_S * MAP_S)
-
-    def put(self, x: int, y: int, state: State):
-        self.data[x + y * MAP_S] = state
-
-    # converts an array of States into an array of bytes
-    def get_bytes(self):
-        return [i.value.to_bytes(1)[0] for i in self.data]
-
+C_COLS = ['red', 'black']
+C_SIZE = 35
 
 app = FastAPI()
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+# entry point
+def main():
+    load_config('config.json')
+    manager = ConnectionManager()
 
-    board = Board()
-    data = [MAP_S, *board.get_bytes()]
-    data = struct.pack('!{}b'.format(len(data)), *data)
-
-    await websocket.send_bytes(data)
+    @app.websocket("/ws")
+    async def websocket_endpoint(websocket: WebSocket):
+        await manager.connect(websocket)
 
 
-@app.get("/config")
-async def send_json():
-    with open("config.json", "r") as file:
-        data = file.read()
-        return data
+class State(Enum):
+    EMPTY = 0
+    BLACK = 1
+    WHITE = 2
+
+
+class Request(Enum):
+    CONFIG = 0
+    UPDATE = 1
+    DRAW = 2
+
+
+class Board:
+    def __init__(self):
+        self.data = [State.EMPTY] * (MAP_S * MAP_S)
+        self.data[0] = State.BLACK
+        self.data[9] = State.BLACK
+        self.data[18] = State.WHITE
+
+    def put(self, x: int, y: int, state: State):
+        self.data[x + y * MAP_S] = state
+
+    # converts an array of States(ints) into an array of single bytes
+    def get_bytes(self):
+        return [i.value.to_bytes(1)[0] for i in self.data]
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+        self.active_boards: dict[WebSocket, Board] = {}
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        board = Board()
+        self.active_boards[websocket] = board
+        await self.send_board(board, websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_board(self, board: Board, websocket: WebSocket):
+        data = board.get_bytes()
+        data = struct.pack('!{}b'.format(len(data)), *data)
+        await websocket.send_bytes(data)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+def load_config(filename):
+    global MAP_S, C_COLS, C_SIZE
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        MAP_S = data['map_dimensions']
+        C_COLS = data['colours']
+        C_SIZE = data['circle_size']
+
+
+if __name__ == "server":
+    main()
--- websock.js
+++ websock.js
@@ -1,8 +1,8 @@
 // Create a new WebSocket connection
 const socket = new WebSocket('ws://localhost:8000/ws');
 
-var map_dimensions = 0;
-var colours = ['white', 'black'];
+let map_dimensions = 8;
+let colours = ['white', 'black'];
 
 load_configuration = function(filename) {
     fetch(filename)
@@ -21,7 +21,7 @@ load_configuration = function(filename) {
 }
 
 // When the connection is open
-socket.onopen = function(event) {
+socket.onopen = function(_event) {
     console.log('WebSocket connection established.');
 };
 
@@ -30,21 +30,18 @@ socket.onmessage = function(event) {
     // Get the text message from the event
     const reader  = new FileReader();
     reader.addEventListener("loadend", function() {
-        const data = new Uint8Array(reader.result);
+        const board = new Uint8Array(reader.result);
 
-        const dimensions = data.slice(0, 1)[0];
-        const board = data.slice(1);
-
-        console.log("Dimensions: ", dimensions);
+        console.log("Dimensions: ", map_dimensions);
         console.log(board);
-        draw_board(dimensions, board);
+        draw_board(map_dimensions, board);
     });
     reader.readAsArrayBuffer(event.data);
 };
 
 draw_board = function(dim, board) {
     // var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-    const circleRadius = 50;
+    const circleRadius = 40;
     const circleSpacing = 2;
 
     canvas.width    = 16 * (circleRadius + 1/2 * circleSpacing);
@@ -52,19 +49,21 @@ draw_board = function(dim, board) {
 
     const ctx = canvas.getContext('2d');
 
-    const gridWidth = canvas.width / (circleRadius * 2 + circleSpacing);
-    const gridHeight = canvas.height / (circleRadius * 2 + circleSpacing);
+    // const gridWidth = canvas.width / (circleRadius * 2 + circleSpacing);
+    // const gridHeight = canvas.height / (circleRadius * 2 + circleSpacing);
 
     // Draw the grid of circles
     for (let row = 0; row < dim; row++) {
         for (let col = 0; col < dim; col++) {
+            const value = board[row + col * dim];
+            if (value == 0) continue;
             const x = col * (circleRadius * 2 + circleSpacing) + circleRadius;
             const y = row * (circleRadius * 2 + circleSpacing) + circleRadius;
 
             // Draw a circle
             ctx.beginPath();
             ctx.arc(x, y, circleRadius, 0, 2 * Math.PI);
-            ctx.fillStyle = 'black';
+            ctx.fillStyle = (value == 1) ? "black" : "red";
             ctx.fill();
             ctx.closePath();
         }
@@ -72,12 +71,12 @@ draw_board = function(dim, board) {
 }
 
 // When the connection is closed
-socket.onclose = function(event) {
+socket.onclose = function(_event) {
     console.log('WebSocket connection closed.');
 };
 
 
 // entry point????
 {
-    load_configuration("config.json");
+    // 
 }
